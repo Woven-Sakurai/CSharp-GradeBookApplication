6455550f4ebd8988a60b4fc47df116bbe18eaafb
Woven-Sakurai <takahiro.sakurai@woven-planet.global>
2021-10-19 19:43:44 +0900

    Completed module 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,7 +20,7 @@ namespace GradeBook.GradeBooks
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-                
+
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
             else if (grades[(threshold * 2) - 1] <= averageGrade)
@@ -32,5 +32,15 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count< 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");                                          
+            }
+            base.CalculateStatistics();
+        }
+      
     }
 }
